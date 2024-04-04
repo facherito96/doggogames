981cf24968fb7d53104f17e75bca36fb4a2f5887
facherito96 <161884590+facherito96@users.noreply.github.com>
2024-04-04 19:43:05 +0200

    a

--- src/sections/Dashboard/WelcomeBanner.tsx
+++ src/sections/Dashboard/WelcomeBanner.tsx
@@ -2,47 +2,11 @@ import React from 'react'
 import styled from 'styled-components'
 import backgroundImg from 'https://github.com/facherito96/doggogames/blob/main/src/sections/Dashboard/try.png'
 
-const Buttons = styled.div`
-  overflow: hidden;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  gap: 10px;
 
-  @media (min-width: 800px) {
-    height: 100%;
-  }
-
-  @media (max-width: 800px) {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    width: 100%;
-    padding-top: 0!important;
-  }
-
-  & > button {
-    border: none;
-    width: 100%;
-    border-radius: 10px;
-    padding: 10px;
-    background: #ffffffdf;
-    transition: background .2s ease;
-    &:hover {
-      background: white;
-    }
-    color: black;
-    cursor: pointer;
-  }
-`
 
 const Welcome = styled.div`
   
   background: url(${backgroundImg});
-  align-items: center;
-  justify-content: center;
-  font-size: 2rem;
 
  
 
