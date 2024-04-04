9158649984be86a4dcd774003bfa81de2f71382b
facherito96 <161884590+facherito96@users.noreply.github.com>
2024-04-04 19:50:29 +0200

    s

--- src/sections/Dashboard/WelcomeBanner.tsx
+++ src/sections/Dashboard/WelcomeBanner.tsx
@@ -1,12 +1,73 @@
 import React from 'react'
 import styled from 'styled-components'
-import backgroundImg from 'https://github.com/facherito96/doggogames/blob/main/src/sections/Dashboard/try.png'
 
+const Buttons = styled.div`
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  gap: 10px;
 
+  @media (min-width: 800px) {
+    height: 100%;
+  }
+
+  @media (max-width: 800px) {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 100%;
+    padding-top: 0!important;
+  }
+
+  & > button {
+    border: none;
+    width: 100%;
+    border-radius: 10px;
+    padding: 10px;
+    background: #ffffffdf;
+    transition: background .2s ease;
+    &:hover {
+      background: white;
+    }
+    color: black;
+    cursor: pointer;
+  }
+`
 
 const Welcome = styled.div`
-  
-  background: url(${backgroundImg});
+  @keyframes welcome-fade-in {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+
+  @keyframes backgroundGradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+
+  background-size: 300% 300%;
+  border-radius: 10px;
+  position: relative;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 20px;
+  font-size: 2rem;
 
  
 
@@ -27,6 +88,10 @@ const Welcome = styled.div`
 export function WelcomeBanner() {
   return (
     <Welcome>
+      <div>
+        <h1>WELCOME TO DOGGO GAMES</h1>
+        <p>the first MEME COIN on SOLANA with a real use case!</p>
+      </div>
     </Welcome>
   )
 }
