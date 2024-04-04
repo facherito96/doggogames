50bb2ac7785eb6a66245894731512ba24ca88950
facherito96 <161884590+facherito96@users.noreply.github.com>
2024-04-04 18:56:42 +0200

    s

--- src/sections/Dashboard/WelcomeBanner.tsx
+++ src/sections/Dashboard/WelcomeBanner.tsx
@@ -98,7 +98,7 @@ const Welcome = styled.div`
 export function WelcomeBanner() {
   return (
     <Welcome>
-      <div><img src="/try.png" alt="" /></div>
+      <div><img src="/Dashboard/try.png" alt="" /></div>
     </Welcome>
   )
 }
