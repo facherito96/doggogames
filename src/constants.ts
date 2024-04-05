2f862484d13f934afb41ab1090d2aedd4615f5ce
facherito96 <161884590+facherito96@users.noreply.github.com>
2024-04-05 11:28:00 +0200

    update gamba

--- src/constants.ts
+++ src/constants.ts
@@ -1,25 +1,87 @@
-import { GambaStandardTokens, TokenMeta } from 'gamba-react-ui-v2'
 import { PublicKey } from '@solana/web3.js'
+import { FAKE_TOKEN_MINT, PoolToken, TokenMeta, makeHeliusTokenFetcher } from 'gamba-react-ui-v2'
 
-// Can be configured in .env
-export const RPC_ENDPOINT = import.meta.env.VITE_RPC_ENDPOINT ?? "https://api.mainnet-beta.solana.com"
+// Get RPC from the .env file or default to the public RPC.
+export const RPC_ENDPOINT = import.meta.env.VITE_RPC_ENDPOINT ?? 'https://api.mainnet-beta.solana.com'
 
-// Change this value to your Solana address
-export const PLATFORM_CREATOR_ADDRESS = new PublicKey('GigeGLQVXCHWrdoV4DoC6ymfTtPCGWcGwnoQyxVuxgEa')
+// Solana address that will receive fees when somebody plays on this platform
+export const PLATFORM_CREATOR_ADDRESS = new PublicKey(
+  'GigeGLQVXCHWrdoV4DoC6ymfTtPCGWcGwnoQyxVuxgEa'
+)
 
-// Appears in ShareModal
-export const PLATFORM_SHARABLE_URL = 'v2-play.gamba.so'
+// Gamba explorer URL - Appears in RecentPlays
+export const EXPLORER_URL = `https://explorer.gamba.so/`;
 
-// List of tokens supported by this platform
-export const TOKENS: TokenMeta[] = [
-  GambaStandardTokens.sol,
-  GambaStandardTokens.usdc,
-  // {
-  //   mint: new PublicKey(""),
-  //   symbol: '???',
-  //   name: 'Custom SPL Token',
-  //   image: "image url",
-  //   decimals: 6,
-  //   baseWager: 1 * 1e6,
-  // }
+// Platform URL - Appears in ShareModal
+export const PLATFORM_SHARABLE_URL = 'play.gamba.so'
+
+// Creator fee (in %)
+export const PLATFORM_CREATOR_FEE = 0.05 // 1% (1/100 = 0.01)
+
+// Jackpot fee (in %)
+export const PLATFORM_JACKPOT_FEE = 0.001 // 0.1% (0.1/100 = 0.001)
+
+// Just a helper function
+const lp = (tokenMint: PublicKey | string, poolAuthority?: PublicKey | string): PoolToken => ({
+  token: new PublicKey(tokenMint),
+  authority: poolAuthority !== undefined ? new PublicKey(poolAuthority) : undefined,
+})
+
+/**
+ * List of pools supported by this platform
+ * Make sure the token you want to list has a corresponding pool on https://explorer.gamba.so/pools
+ * For private pools, add the creator of the Liquidity Pool as a second argument
+ */
+export const POOLS = [
+  // SOL:
+  lp('So11111111111111111111111111111111111111112'),
+  // USDC:
+  lp('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'),
+  // Fake token:
+  lp(FAKE_TOKEN_MINT),
+  // Wormhole:
+  lp('85VBFQZC9TZkfaptBWjvUw7YbZjy52A6mjtPGjstQAmQ'),
 ]
+
+// The default token to be selected
+export const DEFAULT_POOL = POOLS[0]
+
+/**
+ * List of token metadata for the supported tokens
+ * Alternatively, we can provide a fetcher method to automatically fetch metdata. See TOKEN_METADATA_FETCHER below.
+ */
+export const TOKEN_METADATA: (Partial<TokenMeta> & {mint: PublicKey})[] = [
+  {
+    mint: FAKE_TOKEN_MINT,
+    name: 'Fake',
+    symbol: 'FAKE',
+    image: '/fakemoney.png',
+    baseWager: 1e9,
+    decimals: 9,
+    usdPrice: 0,
+  },
+  {
+    mint: new PublicKey('85VBFQZC9TZkfaptBWjvUw7YbZjy52A6mjtPGjstQAmQ'),
+    name: 'W',
+    symbol: 'Wormhole',
+    image: 'https://wormhole.com/token.png',
+    baseWager: 1e6,
+    decimals: 6,
+    usdPrice: 0,
+  },
+]
+
+/**
+ * A method for automatically fetching Token Metadata.
+ * Here we create a fetcher that uses Helius metadata API, if an API key exists as an environment variable.
+ */
+export const TOKEN_METADATA_FETCHER = (
+  () => {
+    if (import.meta.env.VITE_HELIUS_API_KEY) {
+      return makeHeliusTokenFetcher(
+        import.meta.env.VITE_HELIUS_API_KEY,
+        { dollarBaseWager: 1 },
+      )
+    }
+  }
+)()
--- src/games/Slots/Slots.styles.ts
+++ src/games/Slots/Slots.styles.ts
@@ -1,106 +1,38 @@
 import styled from 'styled-components'
 
-export const StyledSlots = styled.div`
-  perspective: 100px;
-  user-select: none;
-
-  & > div {
-    display: grid;
-    gap: 20px;
-    transform: rotateX(3deg) rotateY(0deg);
-  }
-
-  @keyframes pulse {
-    0%, 30% {
-      transform: scale(1)
-    }
-    10% {
-      transform: scale(1.3)
-    }
-  }
-
-  @keyframes reveal-glow {
-    0%, 30% {
-      border-color: #2d2d57;
-      background: #ffffff00;
-    }
-    10% {
-      border-color: white;
-      background: #ffffff33;
-    }
-  }
-
-  @keyframes shine {
-    0%, 30% {
-      background: #ffffff00;
-    }
-    10% {
-      background: #ffffff33;
-    }
-  }
-
-  @keyframes result-flash {
-    25%, 75% {
-      background-color: #ffec63;
-      color: #333;
-    }
-    50% {
-      background-color: #ffec6311;
-      color: #ffec63;
-    }
-  }
-
-  .result {
-    border: none;
-    padding: 10px;
-    text-transform: uppercase;
-    position: relative;
-    padding: 10px;
-    width: 100%;
-    border-radius: 10px;
-    border-spacing: 10px;
-    border: 1px solid #ffec63;
-    background-color: #ffec6311;
-    color: #ffec63;
-    font-size: 14px;
-    font-weight: bold;
-    text-align: center;
-  }
-
-  .result[data-good="true"] {
-    animation: result-flash 5s infinite;
-  }
-
-  .slots {
-    display: flex;
-    gap: 20px;
-    justify-content: center;
-    box-sizing: border-box;
-    border-radius: 10px;
-  }
-
-  .slot::after {
-    content: "";
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    z-index: 1;
-  }
-
-  @keyframes reveal {
+export const StyledSpinner = styled.div`
+  @keyframes spinning {
     0% {
-      transform: translateY(100%);
-      opacity: 0;
+      top: 0;
     }
     100% {
-      transform: translateY(0%);
-      opacity: 1;
+      top: calc(var(--num-items) * -100%);
     }
   }
 
-  .slotImage {
-    aspect-ratio: 1/1;
-    max-width: 100%;
-    max-height: 100%;
+  --num-items: 5;
+  --spin-speed: .6s;
+
+  position: absolute;
+  width: 100%;
+  height: 100%;
+
+  transition: opacity .1s .1s ease;
+  animation: spinning var(--spin-speed) .1s linear infinite;
+  opacity: 0;
+
+  &[data-spinning="true"] {
+    opacity: 1;
   }
+
+  & > div {
+    color: white;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    padding: 15px;
+  }
+
 `
