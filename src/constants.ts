68a5a6152a6a068819d0a49f20615f9ecbf6ff58
facherito96 <161884590+facherito96@users.noreply.github.com>
2024-04-09 19:14:39 +0200

    j

--- src/constants.ts
+++ src/constants.ts
@@ -33,12 +33,13 @@ const lp = (tokenMint: PublicKey | string, poolAuthority?: PublicKey | string): 
  * For private pools, add the creator of the Liquidity Pool as a second argument
  */
 export const POOLS = [
+   // Fake token:
+   lp(FAKE_TOKEN_MINT),
   // SOL:
   lp('So11111111111111111111111111111111111111112'),
   // USDC:
   lp('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'),
-  // Fake token:
-  lp(FAKE_TOKEN_MINT),
+ 
   // Wormhole:
   lp('85VBFQZC9TZkfaptBWjvUw7YbZjy52A6mjtPGjstQAmQ'),
 ]
