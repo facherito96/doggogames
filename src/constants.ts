250c8dbd601f4d94de89338efe75974e01336dc6
facherito96 <161884590+facherito96@users.noreply.github.com>
2024-05-01 17:33:53 +0100

    S

--- src/constants.ts
+++ src/constants.ts
@@ -56,7 +56,7 @@ export const DEFAULT_POOL = POOLS[0]
 export const TOKEN_METADATA: (Partial<TokenMeta> & {mint: PublicKey})[] = [
   {
     mint: new PublicKey('BVNjgfQRVbtWRYL9WbPiDA9eXQeTxq3847aSJmUAU2Pj'),
-    name: 'DOGGO',
+    name: 'DOGGO GAMES',
     symbol: 'DOGGO$',
     image: 'https://cdn.discordapp.com/attachments/1233523783445385318/1235183039215046686/doggo_games.png?ex=66337159&is=66321fd9&hm=55c2812ad40f1b04f1bdb5805ac79c27c20538434abf6d137aa1773050346f4b&',
     baseWager: 1e9,
